106c1b5a4c0c4eebc2372373594cac84fbb8c8b0
mrlado <33937546+mrlado@users.noreply.github.com>
2023-04-25 23:01:30 +0300

    First upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Video from './Assats/Earth1.mp4'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main w-full h-full'>
+      <div className='overlayer'></div>
+        <video autoPlay loop muted id='video w-full h-full object-cover'>
+          <source src={Video}/>
+        </video>
+        <div className='content absolute w-full h-full top-0 flex flex-column justify-center items-center z-2 '>
+            <h1>Welcome</h1>
+            <p>Lado Merabishvili</p>
+        </div>
     </div>
   );
 }
